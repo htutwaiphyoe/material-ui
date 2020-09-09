9d53f74c7863ba54ce92fd4c133a61307d246a47
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-09 20:32:35 +0630

    JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+    const changeCurrentTime = () => {
+        setCurrentTime(new Date().toLocaleTimeString());
+    };
+    useEffect(() => {
+        changeCurrentTime();
+    }, [currentTime]);
+    const styleDiv = {
+        fontSize: "50px",
+        color: "#111",
+        fontWeight: "bold",
+    };
+    return (
+        <div className="App">
+            <div style={styleDiv}>{currentTime}</div>
+        </div>
+    );
 }
 
 export default App;
