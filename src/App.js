ab323d39a94beb27d85e392814524f48ee711447
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-11 21:49:44 +0630

    Props children

--- src/App.js
+++ src/App.js
@@ -2,30 +2,39 @@ import React from "react";
 import faker from "faker";
 
 import Comment from "./Comment/Comment";
+import ApprovalCard from "./ApprovalCard/ApprovalCard";
 import "./App.css";
 
 const App = () => {
     const date = new Date().toLocaleTimeString();
     return (
-        <div className="ui container comments">
-            <Comment
-                image={faker.image.avatar}
-                name={faker.name.findName}
-                date={date}
-                comment="Congrats"
-            />
-            <Comment
-                image={faker.image.avatar}
-                name={faker.name.findName}
-                date={date}
-                comment="Congrats again"
-            />
-            <Comment
-                image={faker.image.avatar}
-                name={faker.name.findName}
-                date={date}
-                comment="Congratulations"
-            />
+        <div className="App">
+            <div className="ui container comments ">
+                <ApprovalCard>
+                    <Comment
+                        image={faker.image.avatar}
+                        name={faker.name.findName}
+                        date={date}
+                        comment="Congrats"
+                    />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <Comment
+                        image={faker.image.avatar}
+                        name={faker.name.findName}
+                        date={date}
+                        comment="Congrats again"
+                    />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <Comment
+                        image={faker.image.avatar}
+                        name={faker.name.findName}
+                        date={date}
+                        comment="Congratulations"
+                    />
+                </ApprovalCard>
+            </div>
         </div>
     );
 };
