7acc00ccd7838710ad23b7708d9ab56a6133eb56
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-11 21:51:07 +0630

    Props children

--- src/App.js
+++ src/App.js
@@ -8,33 +8,31 @@ import "./App.css";
 const App = () => {
     const date = new Date().toLocaleTimeString();
     return (
-        <div className="App">
-            <div className="ui container comments ">
-                <ApprovalCard>
-                    <Comment
-                        image={faker.image.avatar}
-                        name={faker.name.findName}
-                        date={date}
-                        comment="Congrats"
-                    />
-                </ApprovalCard>
-                <ApprovalCard>
-                    <Comment
-                        image={faker.image.avatar}
-                        name={faker.name.findName}
-                        date={date}
-                        comment="Congrats again"
-                    />
-                </ApprovalCard>
-                <ApprovalCard>
-                    <Comment
-                        image={faker.image.avatar}
-                        name={faker.name.findName}
-                        date={date}
-                        comment="Congratulations"
-                    />
-                </ApprovalCard>
-            </div>
+        <div className="ui container comments ">
+            <ApprovalCard>
+                <Comment
+                    image={faker.image.avatar}
+                    name={faker.name.findName}
+                    date={date}
+                    comment="Congrats"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment
+                    image={faker.image.avatar}
+                    name={faker.name.findName}
+                    date={date}
+                    comment="Congrats again"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment
+                    image={faker.image.avatar}
+                    name={faker.name.findName}
+                    date={date}
+                    comment="Congratulations"
+                />
+            </ApprovalCard>
         </div>
     );
 };
