6e1a821604cb4ec81716da536a505639eee51c4c
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-17 22:10:03 +0630

    props

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import faker from "faker";
+
+import Comment from "./Comment/Comment";
+import ApprovalCard from "./ApprovalCard/ApprovalCard";
+import "./App.css";
+
+const App = () => {
+    const date = new Date().toLocaleTimeString();
+    return (
+        <div className="ui container comments ">
+            <ApprovalCard>
+                <Comment
+                    image={faker.image.avatar}
+                    name={faker.name.findName}
+                    date={date}
+                    comment="Congrats"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <Comment
+                    image={faker.image.avatar}
+                    name={faker.name.findName}
+                    date={date}
+                    comment="Congratulations"
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+export default App;
