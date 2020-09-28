c27bc51306de6052056edf53fe48d9a3d6d1ce4a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-28 22:46:20 +0630

    App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-    return <div>Hello, world</div>;
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
 };
 
 export default App;
