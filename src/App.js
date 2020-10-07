95d472b907bc9fba1b599765d56fd46e22aa5f83
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-07 18:37:13 +0630

    Finishing Season Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import SeasonApp from "./Container/Season/SeasonApp";
 class App extends React.Component {
     state = {};
 
     render() {
-        return <SeasonApp />;
+        return <div>Hello World</div>;
     }
 }
 
