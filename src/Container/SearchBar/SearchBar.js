2941d6b3f0b56d8c77765ac002d5c209be70099a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-11 23:59:21 +0630

    Search Event Handler

--- src/Container/SearchBar/SearchBar.js
+++ src/Container/SearchBar/SearchBar.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import classes from "./SearchBar.module.css";
 class Search extends Component {
-    test = (e) => {
+    state = {
+        input: "",
+    };
+    onInputChange = (e) => {
+        this.setState({ input: e.target.value });
+    };
+    onFormSubmit = (e) => {
         e.preventDefault();
-        console.log(e);
+        console.log(this.state.input);
     };
     render() {
         return (
             <div className={`ui segment ${classes.SearchBar}`}>
-                <form onSubmit={this.test} className={`ui form`}>
+                <form onSubmit={this.onFormSubmit} className={`ui form`}>
                     <div className={`ui icon input ${classes.input}`}>
-                        <input type="text" placeholder="Search..." />
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            onChange={this.onInputChange}
+                            value={this.state.input}
+                        />
                         <i className="search icon"></i>
                     </div>
                 </form>
